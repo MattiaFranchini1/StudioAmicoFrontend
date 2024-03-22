07a7ca8cdf4151bfa0de3b2aee405f312b8e6321
Mattia Franchini <franchini.mattia.studente@itispaleocapa.it>
2024-03-22 16:18:59 +0100

    vite.config.js: converted in https server

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
+    port: 5000, 
+    host: '0.0.0.0',
+    https: {
+      key: process.env.PRIV_KEY,
+      cert: process.env.CERT
+    }
   }
 })
